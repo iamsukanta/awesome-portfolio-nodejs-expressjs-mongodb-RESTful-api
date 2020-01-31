8bfc70b8fbdee5171db36c1a77d946db4391bb4b
Sukanta Bala <sukantabalacste2016@gmail.com>
2020-01-31 21:57:37 +0600

    User api endpoint updated

--- app/http/routes/users.route.js
+++ app/http/routes/users.route.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const usersapiAction = require('../actions/usersapi.action')
 
 router.get('/seed', usersapiAction.createSeedingUser)
-	// .post('/', usersapiAction.create)
-	// .get('/', usersapiAction.list)
-	// .get('/:id', usersapiAction.show)
-	// .put('/:id', usersapiAction.update)
-	// .delete('/:id', usersapiAction.destroy)
+	.post('/create', usersapiAction.create)
+	.get('/', usersapiAction.list)
+	.get('/:id', usersapiAction.show)
+	.put('/:id', usersapiAction.update)
+	.delete('/:id', usersapiAction.destroy)
 
 module.exports = router
--- app/services/usersapi.service.js
+++ app/services/usersapi.service.js
@@ -4,14 +4,25 @@ const axios = require('axios');
 User = require('../models/user.js');
 
 module.exports = {
-    // create: (req, res) => {
-    //     var user = new User();
-    //     return user.save()
-    //     .then(function(response) { 
-    //         res.json(response); 
-    //     })
-    //     .error(function(err) { res.send(err); });     
-    // },
+    create: async (req, res) => {
+        let existUser = await User.find({ email: req.body.email});
+        if(existUser.length < 1) {
+            var user = new User();
+            user.name = req.body.name;
+            user.email = req.body.email;
+            user.password = req.body.password;
+            return user.save()
+            .then(function(response) { 
+                res.status(200).json({ message: "User Successfully Created."}); 
+            }).catch(err => {
+                res.status(400).json({ message: err});
+            });    
+            
+        } else {
+            res.status(400).json({ message: 'Eamil Already Exists'});
+        }
+        
+    },
 
     createSeedingUser: async(req, res) => {
         let seedingUser = await require ('../utils/userseedingdata.js');
@@ -33,46 +44,50 @@ module.exports = {
         })     
     },
 
-    // update: (req, res) => {
-    //     var user = new User();
-    //     var id = req.params.id;
-    //     return user.findById(id)
-    //         .exec((err, user) => {
-    //         if (err) { res.status(400).json(err);}
-    //         else {
-    //             user.save()
-    //             .then(function(response) { 
-    //                 res.json(response); 
-    //             })
-    //         }
-    //     });
-    // },
+    update: (req, res) => {
+        var id = req.params.id;
+        return User.findById(id)
+            .exec((err, user) => {
+            if (err) { res.status(400).json(err);}
+            else {
+                user.name = req.body.name;
+                user.email = req.body.email;
+                user.password = req.body.password;
+                user.save()
+                .then(function(response) { 
+                    res.status(200).json({message: "User successfylly updated.", success: true}) 
+                }).catch(err => {
+                    res.status(400).json({ message: err});
+                });  
+            }
+        });
+    },
 
-    // list: (req, res) => {
-    //     return User.find({})
-    //     .sort({created_at: -1})
-    //     .exec((err, user) => {
-    //         if (err) res.status(400).json(err);
-    //         res.status(200).json({data: user, success: true})
-    //     });
-    // },
+    list: (req, res) => {
+        return User.find({})
+        .sort({created_at: -1})
+        .exec((err, user) => {
+            if (err) res.status(400).json(err);
+            res.status(200).json({data: user, success: true})
+        });
+    },
 
 
-    // show: (req, res) => {
-    //     var id = req.params.id;
-    //     return User.findById(id)
-    //         .exec((err, user) => {
-    //         if (err) res.status(400).json(err);
-    //         res.status(200).json({data: user, success: true})
-    //     });
-    // },
+    show: (req, res) => {
+        var id = req.params.id;
+        return User.findById(id)
+            .exec((err, user) => {
+            if (err) res.status(400).json(err);
+            res.status(200).json({data: user, success: true})
+        });
+    },
 
-    // destroy: (req, res) => {
-    //     var id = req.params.id;
-    //     return User.findByIdAndRemove(id)
-    //         .exec((err, sample) => {
-    //         if (err) res.status(400).json(err);
-    //         res.status(200).json({data: {message: 'Uber Service successfully deleted'}, success: true})
-    //     });
-    // }
+    destroy: (req, res) => {
+        var id = req.params.id;
+        return User.findByIdAndRemove(id)
+            .exec((err, sample) => {
+            if (err) res.status(400).json(err);
+            res.status(200).json({data: {message: 'User successfully deleted'}, success: true})
+        });
+    }
 }
